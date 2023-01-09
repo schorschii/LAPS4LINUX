2270d6062495421a33045ba96812eec98cd0fc6c
Georg Sieber <it@georg-sieber.de>
2023-01-09 19:52:59 +0100

    improve build script

--- setup/macos/build.sh
+++ setup/macos/build.sh
@@ -2,29 +2,34 @@
 
 cd "$(dirname "$0")"
 
+DMG_FILE_TMP_MOUNT="/Volumes/LAPS4MAC"
+DMG_FILE_TMP="laps4mac-rw.dmg"
+DMG_FILE="laps4mac.dmg"
+
 # remove temp build folder
 rm -r "../../dist/LAPS4MAC"
 
 # check if mount point is free
-if [ -d "/Volumes/LAPS4MAC" ]; then
-	echo "ERROR: /Volumes/LAPS4MAC already mounted"
+if [ -d "$DMG_FILE_TMP_MOUNT" ]; then
+	echo "ERROR: $DMG_FILE_TMP_MOUNT already mounted"
 	exit 1
 fi
 
 # create DMG with .app directory and /Applications link
 rm "../../dist/.DS_Store"
-hdiutil create -srcfolder "../../dist" -volname "LAPS4MAC" -fs HFS+ -fsargs "-c c=64,a=16,e=16" -format UDRW LAPS4MAC-rw.dmg
-hdiutil attach -readwrite -noverify -noautoopen LAPS4MAC-rw.dmg
-ln -s /Applications /Volumes/LAPS4MAC/Applications
+hdiutil create -srcfolder "../../dist" -volname "LAPS4MAC" -fs HFS+ -fsargs "-c c=64,a=16,e=16" -format UDRW "$DMG_FILE_TMP"
+hdiutil attach -readwrite -noverify -noautoopen "$DMG_FILE_TMP"
+ln -s "/Applications" "$DMG_FILE_TMP_MOUNT/Applications"
 
 # set volume icon
-cp "../../assets/setup.icns" "/Volumes/LAPS4MAC/.VolumeIcon.icns"
-SetFile -c icnC "/Volumes/LAPS4MAC/.VolumeIcon.icns"
-SetFile -a C "/Volumes/LAPS4MAC"
+cp "../../assets/setup.icns" "$DMG_FILE_TMP_MOUNT/.VolumeIcon.icns"
+SetFile -c icnC "$DMG_FILE_TMP_MOUNT/.VolumeIcon.icns"
+SetFile -a C "$DMG_FILE_TMP_MOUNT"
 
 # create final DMG
 sleep 1
-hdiutil detach /Volumes/LAPS4MAC
+rm -rf "$DMG_FILE_TMP_MOUNT/.fseventsd"
+hdiutil detach "$DMG_FILE_TMP_MOUNT"
 sleep 1
-hdiutil convert LAPS4MAC-rw.dmg -format UDZO -o LAPS4MAC.dmg
-rm LAPS4MAC-rw.dmg
+hdiutil convert "$DMG_FILE_TMP" -format UDZO -o "$DMG_FILE"
+rm "$DMG_FILE_TMP"
