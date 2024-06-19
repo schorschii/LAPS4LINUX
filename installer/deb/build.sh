3f585fe6573cee1559f6a4df268972b47550c581
fvollmer <felixvollmer@gmail.com>
2024-06-19 17:43:11 +0200

    allow building deb packages without root

--- installer/deb/build.sh
+++ installer/deb/build.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 set -e
 
-# check root permissions
-if [ "$EUID" -ne 0 ] && ! groups | grep -q sudo ; then
-	echo "Please run this script as root!"
-	#exit 1 # disabled for github workflow. don't know why this check fails here but sudo works.
-fi
-
 # cd to working dir
 cd "$(dirname "$0")"
 
@@ -17,24 +11,21 @@ BUILDDIR=laps4linux-client
 
 # empty / create necessary directories
 if [ -d "$BUILDDIR/usr" ]; then
-	sudo rm -r $BUILDDIR/usr
+        rm -r $BUILDDIR/usr
 fi
 
 # copy files in place
-sudo install -D -m 644 ../../README.md                         -t $BUILDDIR/$INSTALLDIR
-sudo install -D -m 644 ../../assets/laps.png                   -t $BUILDDIR/usr/share/pixmaps
-sudo install -D -m 644 ../../assets/LAPS4LINUX.desktop         -t $BUILDDIR/usr/share/applications
-sudo install -D -m 644 ../../laps-client/laps_client/*.py      -t $BUILDDIR/$INSTALLDIR/laps_client
-sudo install -D -m 644 ../../laps-client/requirements.txt      -t $BUILDDIR/$INSTALLDIR
-sudo install -D -m 644 ../../laps-client/setup.py              -t $BUILDDIR/$INSTALLDIR
-
-# set file permissions
-sudo chown -R root:root $BUILDDIR
+install -D -m 644 ../../README.md                         -t $BUILDDIR/$INSTALLDIR
+install -D -m 644 ../../assets/laps.png                   -t $BUILDDIR/usr/share/pixmaps
+install -D -m 644 ../../assets/LAPS4LINUX.desktop         -t $BUILDDIR/usr/share/applications
+install -D -m 644 ../../laps-client/laps_client/*.py      -t $BUILDDIR/$INSTALLDIR/laps_client
+install -D -m 644 ../../laps-client/requirements.txt      -t $BUILDDIR/$INSTALLDIR
+install -D -m 644 ../../laps-client/setup.py              -t $BUILDDIR/$INSTALLDIR
 
 # make binaries available in PATH
-sudo mkdir -p $BUILDDIR/usr/bin
-sudo ln -sf   $INSTALLDIR/venv/bin/laps-gui     $BUILDDIR/usr/bin/laps-gui
-sudo ln -sf   $INSTALLDIR/venv/bin/laps-cli     $BUILDDIR/usr/bin/laps-cli
+mkdir -p $BUILDDIR/usr/bin
+ln -sf   $INSTALLDIR/venv/bin/laps-gui     $BUILDDIR/usr/bin/laps-gui
+ln -sf   $INSTALLDIR/venv/bin/laps-cli     $BUILDDIR/usr/bin/laps-cli
 
 
 # build runner .deb package
@@ -43,33 +34,30 @@ BUILDDIR=laps4linux-runner
 
 # empty / create necessary directories
 if [ -d "$BUILDDIR/usr" ]; then
-	sudo rm -r $BUILDDIR/usr
+        rm -r $BUILDDIR/usr
 fi
 
 # copy files in place
-sudo install -D -m 644 ../../README.md                         -t $BUILDDIR/$INSTALLDIR
-sudo install -D -m 644 ../../assets/laps-runner.cron              $BUILDDIR/etc/cron.hourly/laps-runner
-sudo install -D -m 644 ../../laps-runner/laps_runner/*.py      -t $BUILDDIR/$INSTALLDIR/laps_runner
-sudo install -D -m 644 ../../laps-runner/requirements.txt      -t $BUILDDIR/$INSTALLDIR
-sudo install -D -m 644 ../../laps-runner/setup.py              -t $BUILDDIR/$INSTALLDIR
+install -D -m 644 ../../README.md                         -t $BUILDDIR/$INSTALLDIR
+install -D -m 644 ../../assets/laps-runner.cron              $BUILDDIR/etc/cron.hourly/laps-runner
+install -D -m 644 ../../laps-runner/laps_runner/*.py      -t $BUILDDIR/$INSTALLDIR/laps_runner
+install -D -m 644 ../../laps-runner/requirements.txt      -t $BUILDDIR/$INSTALLDIR
+install -D -m 644 ../../laps-runner/setup.py              -t $BUILDDIR/$INSTALLDIR
 # test if we have our own laps-runner config
 if [ -f ../../laps-runner/laps-runner.json ]; then
-	sudo install -D -m 644 ../../laps-runner/laps-runner.json         $BUILDDIR/etc/laps-runner.json
+        install -D -m 644 ../../laps-runner/laps-runner.json         $BUILDDIR/etc/laps-runner.json
 else
-	echo 'WARNING: You are using the example json config file, make sure this is intended'
-	sudo install -D -m 644 ../../laps-runner/laps-runner.json.example $BUILDDIR/etc/laps-runner.json
+        echo 'WARNING: You are using the example json config file, make sure this is intended'
+        install -D -m 644 ../../laps-runner/laps-runner.json.example $BUILDDIR/etc/laps-runner.json
 fi
 
-# set file permissions
-sudo chown -R root:root $BUILDDIR
-
 # make binaries available in PATH
-sudo mkdir -p $BUILDDIR/usr/sbin
-sudo ln -sf   $INSTALLDIR/venv/bin/laps-runner     $BUILDDIR/usr/sbin/laps-runner
+mkdir -p $BUILDDIR/usr/sbin
+ln -sf   $INSTALLDIR/venv/bin/laps-runner     $BUILDDIR/usr/sbin/laps-runner
 
 
 # build debs
-sudo dpkg-deb -Zxz --build laps4linux-client
-sudo dpkg-deb -Zxz --build laps4linux-runner
+dpkg-deb -Zxz --root-owner-group --build laps4linux-client
+dpkg-deb -Zxz --root-owner-group --build laps4linux-runner
 
 echo "Build finished"
