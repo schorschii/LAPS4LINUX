1ba85b51beadd5f37f50bdbc4401482d6915dc62
Georg Sieber <Georg.Sieber@slub-dresden.de>
2024-08-06 16:09:25 +0200

    bump version

--- installer/rpm/rpmbuild/SPECS/laps4linux-client.spec
+++ installer/rpm/rpmbuild/SPECS/laps4linux-client.spec
@@ -1,5 +1,5 @@
 Name:           laps4linux-client
-Version:        1.9.0
+Version:        1.10.0
 Release:        1%{?dist}
 Summary:        Laps4linux - auto-rotate the root password for AD bound (samba net, pbis, adcli) linux servers
 BuildArch:      noarch
--- laps-runner/laps_runner/__init__.py
+++ laps-runner/laps_runner/__init__.py
@@ -2,7 +2,7 @@ __title__     = 'LAPS4LINUX'
 __author__    = 'Georg Sieber'
 __copyright__ = '© 2021-2024'
 __license__   = 'GPL-3.0'
-__version__   = '1.9.0'
+__version__   = '1.10.0'
 __website__   = 'https://github.com/schorschii/LAPS4LINUX'
 
 __all__ = [__author__, __license__, __version__]
