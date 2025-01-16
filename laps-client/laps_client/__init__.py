17b38086e2af200d336b4d779e0507dc5c5f6881
Georg Sieber <it@georg-sieber.de>
2025-01-16 13:35:45 +0100

    cert fix for macOS build on Github runners

--- laps-client/laps_client/__init__.py
+++ laps-client/laps_client/__init__.py
@@ -9,8 +9,18 @@ __all__ = [__author__, __license__, __version__]
 
 
 
+import os, sys
 import getpass
 
+
+if 'darwin' in sys.platform.lower():
+	# set OpenSSL path to macOS defaults
+	# (Github Runner sets this to /usr/local/etc/openssl@1.1/ which does not exist in plain macOS installations)
+	os.environ['SSL_CERT_FILE'] = '/private/etc/ssl/cert.pem'
+	os.environ['SSL_CERT_DIR']  = '/private/etc/ssl/certs'
+	# system CA certs debugging
+	#import ssl; print(ssl.get_default_verify_paths())
+	#ctx = ssl.SSLContext(); ctx.load_default_certs(); print(ctx.get_ca_certs())
 
 def proposeUsername(domain):
 	return getpass.getuser() + ('@'+domain if domain else '')
