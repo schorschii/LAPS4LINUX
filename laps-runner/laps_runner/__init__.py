01c1917df2111fd18c4b52bf6385adfeb88fe651
Georg Sieber <it@georg-sieber.de>
2026-08-01 16:16:58 +0200

    bump version

--- laps-runner/laps_runner/__init__.py
+++ laps-runner/laps_runner/__init__.py
@@ -1,8 +1,8 @@
 __title__     = 'LAPS4LINUX'
 __author__    = 'Georg Sieber'
-__copyright__ = '© 2021-2025'
+__copyright__ = '© 2021-2026'
 __license__   = 'GPL-3.0'
-__version__   = '1.14.0'
+__version__   = '1.15.0'
 __website__   = 'https://github.com/schorschii/LAPS4LINUX'
 
 __all__ = [__author__, __license__, __version__]
